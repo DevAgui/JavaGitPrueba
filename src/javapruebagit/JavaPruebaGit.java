5384197a223860c221b35a45f47e2e83dc499885
dagma <dagma@torrediego>
2022-05-15 18:51:28 +0100

    Añadido el Hola mundo

--- src/javapruebagit/JavaPruebaGit.java
+++ src/javapruebagit/JavaPruebaGit.java
@@ -15,6 +15,8 @@ public class JavaPruebaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.print("Hola mundo"); 
     }
     
 }
